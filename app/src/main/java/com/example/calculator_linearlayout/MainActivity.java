6715700b9b2a35c0201471297d3251bff59c35fb
AlphaEngg <markindustries@gmail.com>
2025-04-19 01:42:09 +0530

    Updated MainActivity.java for button references

--- app/src/main/java/com/example/calculator_linearlayout/MainActivity.java
+++ app/src/main/java/com/example/calculator_linearlayout/MainActivity.java
@@ -3,14 +3,41 @@ package com.example.calculator_linearlayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare all UI components
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    private Button btnAdd, btnSub, btnDiv, btnMul, btnCal, btnClear;
+    private EditText txtCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnAdd,btnSub,btnDiv,btnMul,btnCal;
+        // Initialize all buttons
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+        btnDiv = findViewById(R.id.btnDiv);
+        btnMul = findViewById(R.id.btnMul);
+        btnCal = findViewById(R.id.btnCal);
+        btnClear = findViewById(R.id.btnClear);
+
+        txtCal = findViewById(R.id.txtCal);
+
+        // TODO: Add button click listeners here later
     }
 }
