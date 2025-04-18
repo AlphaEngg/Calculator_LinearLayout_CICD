7b4735afc480f24ca6f1f25a666196c0f7ccffe2
Mark <markindustries@gmail.com>
2025-04-18 18:14:36 +0530

    First commit - Android Java App setup

---
+++ app/src/main/java/com/example/calculator_linearlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.calculator_linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnAdd,btnSub,btnDiv,btnMul,btnCal;
+    }
+}
