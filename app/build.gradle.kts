229db0da81b91c36a7d51ed2aa9b6c017aa15a14
AlphaEngg <markindustries@gmail.com>
2025-04-19 03:49:04 +0530

    Integrated App Center SDK for analytics and crash reporting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,10 +32,15 @@ android {
 }
 
 dependencies {
+    val appCenterSdkVersion = "5.0.4"
 
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+
+    implementation("com.microsoft.appcenter:appcenter-analytics:$appCenterSdkVersion")
+    implementation("com.microsoft.appcenter:appcenter-crashes:$appCenterSdkVersion")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/com/example/calculator_linearlayout/MainActivity.java
+++ app/src/main/java/com/example/calculator_linearlayout/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCenter.start(getApplication(), "{Your app secret here}",
+                Analytics.class, Crashes.class);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
